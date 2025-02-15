bc3d773b362832d0cde7a28e1b75d43796f16834
KiagusNaufal <kiagusnaufal310@gmail.com>
2025-02-15 20:48:36 +0700

    apply template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\BarangController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('barang', BarangController::class);
+Route::get(
+    '/',
+    function () {
+        return view('home');
+    }
+);
